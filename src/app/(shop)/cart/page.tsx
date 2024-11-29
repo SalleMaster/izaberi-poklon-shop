289f08a891aed72092d8fe664ef4a8600ed19c66
SalleMaster <salle90ar@gmail.com>
2024-11-29 14:29:15 +0100

    cart page logged in guard

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,27 +1,15 @@
 import { Suspense } from 'react'
 import { getCart } from '@/data/services/cart'
-// import CartTable, {
-//   CartTableSkeleton,
-// } from './_components/cart-table/CartTable'
-// import CartOverview, {
-//   CartOverviewSkeleton,
-// } from './_components/cart-overview/CartOverview'
 import CartPage, { CartPageSkeleton } from './CartPage'
+import pageGuard from '@/lib/pageGuard'
 
 export default async function Page() {
   const cartPromise = getCart()
+  await pageGuard({ callbackUrl: '/korpa' })
 
   return (
     <Suspense fallback={<CartPageSkeleton />}>
       <CartPage cartPromise={cartPromise} />
     </Suspense>
-    // <div className='space-y-5'>
-    //   <Suspense fallback={<CartTableSkeleton />}>
-    //     <CartTable cartPromise={cartPromise} />
-    //   </Suspense>
-    //   <Suspense fallback={<CartOverviewSkeleton />}>
-    //     <CartOverview cartPromise={cartPromise} />
-    //   </Suspense>
-    // </div>
   )
 }
