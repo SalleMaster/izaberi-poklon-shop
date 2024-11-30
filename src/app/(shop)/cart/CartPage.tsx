dde60d64db3233c389da35359d7ebab5d45a378a
SalleMaster <salle90ar@gmail.com>
2024-11-30 14:45:59 +0100

    cart coupon form and apply

--- src/app/(shop)/cart/CartPage.tsx
+++ src/app/(shop)/cart/CartPage.tsx
@@ -3,6 +3,7 @@
 import { use, useOptimistic, useTransition } from 'react'
 import { GetCartReturnType } from '@/data/services/cart'
 import {
+  cartApplyCoupon,
   removeCartItem,
   removeCartItemType,
   updateCartItem,
@@ -16,6 +17,10 @@ import CartOverview, {
 } from './_components/cart-overview/CartOverview'
 import { useToast } from '@/hooks/use-toast'
 import { cn } from '@/lib/utils'
+import {
+  CartCouponForm,
+  CartCouponFormSkeleton,
+} from './_components/cart-coupon-form/CartCouponForm'
 
 type Props = {
   cartPromise: GetCartReturnType
@@ -103,10 +108,38 @@ export default function CartPage({ cartPromise }: Props) {
     }
   }
 
+  const applyCouponHandler = async (data: { coupon: string }) => {
+    try {
+      startTransition(() => {})
+      const response = await cartApplyCoupon(data)
+
+      if (response) {
+        if (response.status === 'fail') {
+          return toast({
+            variant: 'destructive',
+            description: response.message,
+          })
+        }
+
+        if (response.status === 'success') {
+          toast({ description: response.message })
+        }
+      }
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        description:
+          error instanceof Error
+            ? error.message
+            : 'Došlo je do greške. Molimo pokušajte ponovo.',
+      })
+    }
+  }
+
   return (
     <div
       className={cn(
-        'grid gap-6 lg:grid-cols-cart lg:gap-6',
+        'grid gap-6 lg:grid-cols-cart',
         isPending && 'animate-pulse'
       )}
     >
@@ -115,14 +148,20 @@ export default function CartPage({ cartPromise }: Props) {
           optimisticCart={optimisticCart}
           updateCartItemHandler={updateCartItemHandler}
           removeCartItemHandler={removeCartItemHandler}
+          disabled={isPending}
         />
       </div>
-      <div className='mb-auto'>
+      <div className='mb-auto space-y-6'>
         <CartOverview
           onlinePrice={optimisticCart?.onlinePrice}
           totalPrice={optimisticCart?.totalPrice}
           discount={optimisticCart?.discount}
           disabled={isPending || optimisticCart?.items.length === 0}
+        />
+        <CartCouponForm
+          disabled={isPending || optimisticCart?.items.length === 0}
+          appliedCoupon={optimisticCart?.coupon?.code}
+          applyCouponHandler={applyCouponHandler}
         />
       </div>
     </div>
@@ -131,12 +170,13 @@ export default function CartPage({ cartPromise }: Props) {
 
 export function CartPageSkeleton() {
   return (
-    <div className='grid gap-6 lg:grid-cols-cart lg:gap-6'>
+    <div className='grid gap-6 lg:grid-cols-cart'>
       <div className='mb-auto'>
         <CartTableSkeleton />
       </div>
-      <div className='mb-auto'>
+      <div className='mb-auto space-y-6'>
         <CartOverviewSkeleton />
+        <CartCouponFormSkeleton />
       </div>
     </div>
   )
