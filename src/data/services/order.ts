ea6ee756758da55b92c66f789753e807a53e3ca4
SalleMaster <salle90ar@gmail.com>
2024-12-04 16:31:36 +0100

    initial order model setup

---
+++ src/data/services/order.ts
@@ -0,0 +1,46 @@
+import 'server-only'
+
+import { unstable_noStore } from 'next/cache'
+import { cache } from 'react'
+import prisma from '@/lib/db'
+import { slow } from '@/lib/slow'
+import { loggedInActionGuard } from '@/lib/actionGuard'
+import { Order } from '@prisma/client'
+import { CartWithRelations } from './cart'
+
+// export type CartItemWithRelations = CartItem & {
+//   product: Product & {
+//     coverImage: Media | null
+//     priceTable: PriceRange[]
+//   }
+// }
+
+// export type CartWithRelations = Cart & {
+//   items: CartItemWithRelations[]
+//   coupon: Coupon | null
+// }
+
+// export type OrderWithRelations = Order & {
+//   cart: CartWithRelations
+// }
+
+export type GetAllOrdersReturnType = Promise<Order[]>
+
+export const getAllOrders = cache(async (): GetAllOrdersReturnType => {
+  console.log('getAllOrders')
+
+  unstable_noStore()
+  await slow(1000)
+
+  await loggedInActionGuard()
+
+  const orders = await prisma.order.findMany({
+    orderBy: { createdAt: 'desc' },
+  })
+
+  orders.map((order) => {
+    console.log(order)
+  })
+
+  return orders
+})
