89af2e4445832f3039946b8a8b10c48ec33f830a
Sasa Radovanovic <44864166+SalleMaster@users.noreply.github.com>
2024-12-27 10:18:53 +0100

    product ratings initial setup (#112)

--- src/app/(shop)/products/[id]/_components/ProductGrid.tsx
+++ src/app/(shop)/products/[id]/_components/ProductGrid.tsx
@@ -8,26 +8,53 @@ import ProductCarousel, {
 import ProductDetails, {
   ProductDetailsSkeleton,
 } from './product-details/ProductDetails'
+import { GetProductRatingsReturnType } from '@/data/services/ratings'
+import ProductRatingOverview from './product-ratings/product-rating-overview/ProductRatingOverview'
+import ProductRatingList from './product-ratings/product-rating-list/ProductRatingList'
 
 type Props = {
   productPromise: GetProductReturnType
+  productRatingsPromise: GetProductRatingsReturnType
   sessionPromise: Promise<Session | null>
 }
 
-export default function ProductGrid({ productPromise, sessionPromise }: Props) {
+export default function ProductGrid({
+  productPromise,
+  productRatingsPromise,
+  sessionPromise,
+}: Props) {
   const product = use(productPromise)
+  const ratings = use(productRatingsPromise)
   const session = use(sessionPromise)
   const user = session?.user
+
+  console.log(ratings)
 
   return (
     <>
       {product ? (
-        <div>
+        <div className='space-y-10'>
           <div className='relative sm:grid sm:grid-cols-2 sm:gap-4 md:gap-10'>
             <ProductCarousel product={product} />
             <ProductDetails product={product} user={user} />
           </div>
-          <div className='h-[1000px]'></div>
+          <div className='space-y-10'>
+            {ratings.length > 0 && (
+              <>
+                <ProductRatingOverview ratings={ratings} />
+                <p className='text-muted-foreground'>
+                  Recenzije proizvoda koje slede mogu da napišu isključivo
+                  korisnici koji su kupili ovaj proizvod u našoj internet
+                  prodavnici. Zahvaljujući utisku i oceni korisnika koji su
+                  kupili ovaj proizvod, svi posetioci našeg sajta sada imaju
+                  realan uvid u prednosti i mane proizvoda zasnovan na
+                  korisničkom iskustvu. Nadamo se da će Vam ova opcija biti od
+                  koristi pri odabiru pravog proizvoda za Vas.
+                </p>
+                <ProductRatingList ratings={ratings} />
+              </>
+            )}
+          </div>
         </div>
       ) : (
         <NotificationAlert
