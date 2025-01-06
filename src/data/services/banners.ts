e441f9a04fd12122910e86ca95f5448406a9eed0
SalleMaster <salle90ar@gmail.com>
2025-01-06 11:22:01 +0100

    landing page banners component

---
+++ src/data/services/banners.ts
@@ -0,0 +1,29 @@
+import 'server-only'
+
+import { unstable_noStore } from 'next/cache'
+import { cache } from 'react'
+import prisma from '@/lib/db'
+import { slow } from '@/lib/slow'
+import { Banner, Media } from '@prisma/client'
+
+export type BannerWithImageType = Banner & {
+  image: Media | null
+}
+
+export type GetActiveBannersReturnType = Promise<BannerWithImageType[]>
+
+export const getActiveBanners = cache(async (): GetActiveBannersReturnType => {
+  console.log('getActiveBanners')
+
+  unstable_noStore()
+  await slow(1000)
+
+  return prisma.banner.findMany({
+    where: {
+      active: true,
+    },
+    include: {
+      image: true,
+    },
+  })
+})
