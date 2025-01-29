4feb3240ff5e3c3b443629b3e1b0465a47bfc723
Sasa Radovanovic <44864166+SalleMaster@users.noreply.github.com>
2025-01-29 14:27:52 +0100

    products sidebar and not-found page (#154)
    * products sidebar and not-found page
    
    * use isPending prop

--- src/app/_components/header/navigation/_components/nav-bottom/_components/CategoriesList.tsx
+++ src/app/_components/header/navigation/_components/nav-bottom/_components/CategoriesList.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import type { Category, Media } from '@prisma/client'
-import { use } from 'react'
+import { usePathname } from 'next/navigation'
+import React, { use, useOptimistic, useTransition } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
 import {
@@ -9,7 +10,7 @@ import {
   DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu'
 import Link from 'next/link'
-import { fallbackImageURL } from '@/lib/consts'
+import { fallbackImageURL, shopInfo } from '@/lib/consts'
 import useCreateQueryString from '@/hooks/use-create-query-string'
 import { cn } from '@/lib/utils'
 import { Skeleton } from '@/components/ui/skeleton'
@@ -23,25 +24,39 @@ type Props = {
 }
 
 export default function CategoriesList({ categoriesPromise }: Props) {
+  const pathname = usePathname()
   const categories = use(categoriesPromise)
   const searchParams = useSearchParams()
-  const selectedCategory = searchParams.getAll('kategorija')
+  const [isPending, startTransition] = useTransition()
+  const [optimisticCategory, setOptimisticCategories] = useOptimistic(
+    searchParams.getAll('kategorija')
+  )
+  // const selectedCategory = searchParams.getAll('kategorija')
 
   const createQueryString = useCreateQueryString(searchParams)
 
   return (
     <>
       {categories.map((category) => (
-        <DropdownMenuItem key={category.id} asChild>
+        <DropdownMenuItem
+          data-pending-products={isPending ? '' : undefined}
+          key={category.id}
+          asChild
+        >
           <Link
             href={`/pokloni?${createQueryString({
               addParams: [{ name: 'kategorija', value: category.slug }],
               removeParams: ['stranica'],
             })}`}
             className={cn(
-              selectedCategory?.includes(category.slug) &&
+              optimisticCategory?.includes(category.slug) &&
                 'bg-accent text-accent-foreground'
             )}
+            onClick={() => {
+              startTransition(() => {
+                setOptimisticCategories([category.slug])
+              })
+            }}
           >
             <div className='w-6 mr-2'>
               <Image
@@ -55,16 +70,36 @@ export default function CategoriesList({ categoriesPromise }: Props) {
           </Link>
         </DropdownMenuItem>
       ))}
-      <DropdownMenuSeparator />
-      <DropdownMenuItem asChild className='text-end'>
+      <DropdownMenuSeparator className='my-4' />
+      <DropdownMenuItem className='text-end' asChild>
         <Link
           href={`/pokloni?${createQueryString({ removeParams: ['kategorija', 'stranica'] })}`}
           className={cn(
-            selectedCategory.length === 0 && 'bg-accent text-accent-foreground'
+            pathname === '/pokloni' &&
+              optimisticCategory.length === 0 &&
+              'bg-accent text-accent-foreground'
           )}
+          onClick={() => {
+            startTransition(() => {
+              setOptimisticCategories([])
+            })
+          }}
         >
           Svi pokloni
         </Link>
+      </DropdownMenuItem>
+      <DropdownMenuItem asChild className='text-end'>
+        <Link
+          href={'/o-nama'}
+          className={cn(
+            pathname === '/o-nama' && 'bg-accent text-accent-foreground'
+          )}
+        >
+          O nama
+        </Link>
+      </DropdownMenuItem>
+      <DropdownMenuItem asChild>
+        <a href={`tel:${shopInfo.phone}`}>Call centar: {shopInfo.phone}</a>
       </DropdownMenuItem>
     </>
   )
