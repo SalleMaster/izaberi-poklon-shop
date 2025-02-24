1623ae979eb5804459d5de6df95cd55908124d08
Sasa Radovanovic <44864166+SalleMaster@users.noreply.github.com>
2025-02-24 12:49:04 +0100

    Testing ci/cd remove test discount field (#199)
    * add test field to discount table (#193)
    
    * discount new field migration file (#195)
    
    * remove discount test field from db (#197)

---
+++ prisma/migrations/20250224113150_remove_discount_test_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `removeme` on the `discounts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "discounts" DROP COLUMN "removeme";
