f3eeaf8c14dadb317dff367ae2c3082c0902f39f
Sasa Radovanovic <44864166+SalleMaster@users.noreply.github.com>
2025-02-24 10:05:42 +0100

    Update gitignore file (#189)
    * Update gitignore file
    
    * Update gitignore and change prod database url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,29 @@
-import { Pool } from '@neondatabase/serverless'
+// import { Pool } from '@neondatabase/serverless'
+// import { PrismaClient } from '@prisma/client'
+// import { PrismaNeon } from '@prisma/adapter-neon'
+
+// const prismaClientSingleton = () => {
+//   const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
+//   const adapter = new PrismaNeon(neon)
+//   return new PrismaClient({ adapter })
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>
+// } & typeof global
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
 import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon'
 
-const prismaClientSingleton = () => {
-  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
-  const adapter = new PrismaNeon(neon)
-  return new PrismaClient({ adapter })
-}
+const prisma = new PrismaClient()
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
