feb935714f670f68848152702cfb798b093fe6f3
SalleMaster <salle90ar@gmail.com>
2025-04-13 12:58:04 +0200

    delivery fees dashboard update

--- src/app/dashboard/(admin)/delivery-fees/DeliveryFeesPage.tsx
+++ src/app/dashboard/(admin)/delivery-fees/DeliveryFeesPage.tsx
@@ -1,24 +1,19 @@
-import prisma from '@/lib/db'
-import { Card } from '@/components/ui/card'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion'
-import { Package } from 'lucide-react'
-import { DeliveryFeeForm } from './_components/DeliveryFeeForm'
-import { DeliveryFee } from '@prisma/client'
 import { NotificationAlert } from '@/components/custom/NotificationAlert'
+import DeliveryFeeCard, {
+  DeliveryFeeCardSkeleton,
+} from './_components/DeliveryFeeCard'
+import { GetDeliveryFeesReturnType } from '@/data/services/deliveryFees'
+import { use } from 'react'
 
-export default async function DeliveryFeesPage() {
-  const deliveryFees = await prisma.deliveryFee.findMany({
-    orderBy: { fee: 'asc' },
-  })
+type Props = {
+  deliveryFeesPromise: GetDeliveryFeesReturnType
+}
+
+export default function DeliveryFeesPage({ deliveryFeesPromise }: Props) {
+  const deliveryFees = use(deliveryFeesPromise)
+
   return (
     <div className='space-y-10'>
-      <h2 className='text-xl font-bold'>Poštarine</h2>
-
       <div className='space-y-3'>
         <h2 className='text-lg font-medium'>Nova</h2>
         <DeliveryFeeCard />
@@ -42,31 +37,20 @@ export default async function DeliveryFeesPage() {
   )
 }
 
-function DeliveryFeeCard({
-  deliveryFee,
-}: {
-  deliveryFee?: DeliveryFee | null
-}) {
+export function DeliveryFeesPageSkeleton() {
   return (
-    <Card className='py-0'>
-      <Accordion type='single' collapsible className='px-4'>
-        <AccordionItem
-          value={deliveryFee?.id || 'create-delivery-fee'}
-          className='border-b-0'
-        >
-          <AccordionTrigger>
-            <div className='flex items-center gap-4'>
-              <Package />
-              <span className='font-semibold'>
-                {deliveryFee?.name || 'Kreiraj cenu poštarine'}
-              </span>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent>
-            <DeliveryFeeForm deliveryFee={deliveryFee} />
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </Card>
+    <div className='space-y-10'>
+      <div className='space-y-3'>
+        <h2 className='text-lg font-medium'>Nova</h2>
+        <DeliveryFeeCardSkeleton />
+      </div>
+
+      <div className='space-y-3'>
+        <h2 className='text-lg font-medium'>Kreirane</h2>
+        <DeliveryFeeCardSkeleton />
+        <DeliveryFeeCardSkeleton />
+        <DeliveryFeeCardSkeleton />
+      </div>
+    </div>
   )
 }
--- src/app/dashboard/(admin)/delivery-fees/page.tsx
+++ src/app/dashboard/(admin)/delivery-fees/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next'
-import DeliveryFeesPage from './DeliveryFeesPage'
+import DeliveryFeesPage, { DeliveryFeesPageSkeleton } from './DeliveryFeesPage'
 import pageGuard from '@/lib/pageGuard'
+import { getDeliveryFees } from '@/data/services/deliveryFees'
+import { Separator } from '@/components/ui/separator'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Poštarine | Admin',
@@ -12,5 +15,17 @@ export default async function Page() {
     adminGuard: true,
   })
 
-  return <DeliveryFeesPage />
+  const deliveryFeesPromise = getDeliveryFees()
+
+  return (
+    <div className='space-y-5'>
+      <h2 className='text-xl font-bold'>Poštarine</h2>
+
+      <Separator />
+
+      <Suspense fallback={<DeliveryFeesPageSkeleton />}>
+        <DeliveryFeesPage deliveryFeesPromise={deliveryFeesPromise} />
+      </Suspense>
+    </div>
+  )
 }
