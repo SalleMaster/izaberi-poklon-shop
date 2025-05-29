bf5d81e3046e5fe58e86add97e9dfe52e459d415
SalleMaster <salle90ar@gmail.com>
2025-05-29 21:17:25 +0200

    privacy policy update

--- src/app/(shop)/privacy-policy/page.tsx
+++ src/app/(shop)/privacy-policy/page.tsx
@@ -9,13 +9,20 @@ export default function Page() {
       <div className='space-y-3'>
         <p>
           Ovim putem Vas obaveštavamo o informacijama koje prikupljamo od Vas
-          kada nam pošaljete upit putem našeg vebsajta. Prikupljajući ove
-          informacije, mi delujemo kao rukovodilac obrade ličnih podataka u
-          skladu sa Zakonom o zaštiti podataka o ličnosti Republike Srbije (Sl.
-          glasnik RS, br. 87/2018) i Opštom uredbom o zaštiti podataka o
-          ličnosti EU (GDPR). U nastavku možete pronaći više informacija o nama,
-          zašto i kako koristimo Vaše podatke i koja su Vaša prava kada je reč o
-          zaštiti ličnih podataka.
+          kada nam pošaljete upit putem našeg websajta Prikupljaju i ove
+          informacije mi delujemo kao rukovodilac obrade ličnih podataka u
+          skladu sa Zakonom o zaštiti podataka o ličnosti Republike Srbije (Sl
+          glasnik RS br 87/2018) i Opštom uredbom o zaštiti podataka o ličnosti
+          EU (GDPR). Ova Politika privatnosti reguliše praksu prikupljanja
+          informacija od korisnika prilikom koriš enja internet stranice
+          http//www.izaberipoklon.com ,kao i njihovo skladištenje u bazama
+          podataka našeg preduze a Red Dot PR Milanovi Zoran.
+        </p>
+        <p>
+          Naša politika privatnosti obuhvata razloge kako i zašto sakupljamo
+          koristimo i štitimo Vaše lične podatke U nastavku možete prona i više
+          informacija o nama zašto i kako koristimo Vaše podatke i koja su Vaša
+          prava kada je reč o zaštiti ličnih podataka
         </p>
       </div>
 
@@ -26,7 +33,7 @@ export default function Page() {
               Vaša porudžbina i{' '}
               <span className='font-semibold'>podaci su potpuno sigurni!</span>{' '}
               Mi u internet prodavnici IZABERI POKLON i preduzeću Red Dot PR
-              Milanović Zoran. cenimo Vašu privatnost, zato o Vašim podacima
+              Milanović Zoran cenimo Vašu privatnost, zato o Vašim podacima
               izuzetno pažljivo brinemo. Posetioci naše interent prodavnice,
               korišćenjem iste, prihvataju da se podaci o njima koriste u skladu
               sa ovom Politikom privatnosti. Kako bismo brzo i uspešno obradili
@@ -48,9 +55,9 @@ export default function Page() {
               informisanje korisnika u skladu sa dobrim poslovnim običajima u
               cilju pružanja vrhunske i kvalitetne usluge. Vaše podatke ćemo
               koristiti isključivo u svrhu tehničke administracije internet
-              prodavnice, kako bi Vam obezbedili pristup posebnim informacijama,
-              ponudama i materijalima ili za direktnu komunikaciju sa Vama.
-              Nikada i ni na koji način nećemo zloupotrebiti Vaše privatne
+              prodavnice, kako bi vam obezbedili pristup posebnim informacijama,
+              ponudama i materilaima ili za direktnu komunikaciju sa Vama.
+              Nikada i ni na koji način nećmo zloupotrebiti Vaše privatne
               informacije. Registracijom ili prijavom na naš servis
               www.izaberipoklon.com dajete saglasnost kao korisnik servisa sa
               opcijom slanja obaveštenja na Vašu adresu, e-adresu(email),
@@ -68,9 +75,9 @@ export default function Page() {
 
           <li>
             <p>
-              Prilikom unošenja podataka o platnoj kartici, poverljive
-              informacija se prenose putem javne mreže u zaštićenoj
-              (kriptovanoj) formi upotrebom SSL protokola i PKI sistema, kao
+              Prilikom unošenja podataka o platnoj kartici poverljive
+              informacija se prenose putem javne mreže u zašti enoj
+              (kriptovanoj) formi upotrebom SSL protokola i PKI sistema kao
               trenutno najsavremenije kriptografske tehnologije. Sigurnost
               podataka prilikom kupovine, garantuje procesor platnih kartica,
               AllSecure doo, pa se tako kompletni proces naplate obavlja na
@@ -122,7 +129,7 @@ export default function Page() {
             </p>
           </li>
 
-          <li>
+          {/* <li>
             <p>
               <span className='font-semibold'>
                 Internet prodavnica koristi kolačiće
@@ -136,7 +143,7 @@ export default function Page() {
               poslao. Jedna od osnovnih svrha kolačića je da obezbedi pogodnosti
               koje će Vam uštedeti vreme.
             </p>
-          </li>
+          </li> */}
 
           <li>
             <p>
@@ -179,44 +186,57 @@ export default function Page() {
           <li>
             <p>
               Sav materijal na ovom internet sajtu (u daljem tekstu “Sajt”) je
-              zaštićen autorskim pravima i svaka neovlašćena upotreba može
+              zašti en autorskim pravima i svaka neovlaš ena upotreba može
               predstavljati kršenje Zakona o autorskom i srodnim pravima, Zakona
-              o žigovima ili drugih zakona iz oblasti intelektualne svojine.
-              Osim ukoliko je drugačije naznačeno na drugom mestu na ovom sajtu,
-              sav sadržaj možete pregledati, kopirati, štampati ili preuzimati
-              isključivo za ličnu, nekomercijalnu upotrebu i u informativne
+              o žigovima ili drugih zakona iz oblasti intelektualne svojine Osim
+              ukoliko je drugačije naznačeno na drugom mestu na ovom sajtu sav
+              sadržaj možete pregledati kopirati štampati ili preuzimati
+              isključivo za ličnu nekomercijalnu upotrebu i u informativne
               svrhe, pod uslovom da sve naznake o autorskim pravima ili drugim
               vlasničkim informacijama koji su sadržani u originalnom dokumentu
-              zadržite u svim kopijama. Ne možete menjati sadržaj ovog sajta ni
-              na koji način, niti ga reprodukovati ili javno prikazivati,
-              izvoditi, distribuirati ili na drugi način koristiti u javne ili
-              komercijalne svrhe. Ne uzimajući u obzir prethodno navedeno, bilo
-              koji softver ili drugi materijal dostupan za preuzimanje ili
-              korišćenje preko ovog sajta koji poseduje sopstvene uslove
-              korišćenja ili licenciranja nalazi se pod zaštitom tih uslova.
-              Ukoliko prekršite bilo koji od ovih Uslova, vaša dozvola za
-              upotrebu sajta će biti povučena i bićete obavezni da odmah
-              uništite sve sadržaje koje ste preuzeli ili odštampali. Osim ako
-              je eksplicitno drugačije naznačeno ovde ili na nekoj drugoj
-              stranici ovog sajta, ništa ovde sadržano, bilo izričito, prećutno,
-              odricanjem ili na drugi način, Vam ne daje pravo da koristite bilo
-              koji deo intelektualne svojine koju ste pronašli na Sajtu.
-            </p>
-          </li>
-
-          <li>
-            <p>
-              Sva plaćanja biće izvršena u lokalnoj valuti Republike Srbije -
-              dinar (RSD). Za informativni prikaz cena u drugim valutama koristi
-              se srednji kurs Narodne Banke Srbije. Iznos za koji će biti
-              zadužena Vaša platna kartica biće izražen u Vašoj lokalnoj valuti
-              kroz konverziju u istu po kursu koji koriste kartičarske
-              organizacije, a koji nama u trenutku transakcije ne može biti
-              poznat. Kao rezultat ove konverzije postoji mogućnost neznatne
-              razlike od originalne cene navedene na našem sajtu.
+              zadržite u svim kopijama Ne možete menjati sadržaj ovog sajta ni
+              na koji način niti ga reprodukovati ili javno prikazivati izvoditi
+              distribuirati ili na drugi način koristiti u javne ili
+              komercijalne svrhe Ne uzimaju i u obzir prethodno navedeno bilo
+              koji softver ili drugi materijal dostupan za preuzimanje ili koriš
+              enje preko ovog sajta koji poseduje sopstvene uslove koriš enja
+              ili licenciranja nalazi se pod zaštitom tih uslova Ukoliko
+              prekršite bilo koji od ovih Uslova vaša dozvola za upotrebu sajta
+              e biti povučena i bi ete obavezni da odmah uništite sve sadržaje
+              koje ste preuzeli ili odštampali Osim ako je eksplicitno drugačije
+              naznačeno ovde ili na nekoj drugoj stranici ovog sajta ništa ovde
+              sadržano bilo izričito pre utno odricanjem ili na drugi način vam
+              ne daje pravo da koristite bilo koji deo intelektualne svojine
+              koju ste pronašli na Sajtu.
             </p>
           </li>
         </ul>
+      </div>
+
+      <div className='space-y-3'>
+        <p className='text-xl font-semibold'>Izjava o konverzijama</p>
+        <p>
+          Sva pla anja e biti obavljenima u dinarima (RSD) Ukoliko se pla a
+          platnim karticama inostranih Banaka izdavalaca dinarski iznos
+          transakcije e biti konvertovan u settlement valutu Banke (EUR) prema
+          kursu Narodne Banke Srbije.
+        </p>
+        <p>
+          Pri zaduživanju Vase platne kartice ve konvertovan iznos e se
+          konvertovati u Vasu lokalnu valutu, prema kursu koji primenjuju
+          operatori platnih kartica.
+        </p>
+        <p>
+          "Please note that all payments will be effected in the Serbian
+          currency - dinar (RSD). If the payment is done using foreign issuers
+          payment cards, total amount of transaction will be converted into bank
+          settlement currency, according to the current exchange rate of the
+          Serbian National Bank. Once when transaction is settled the already
+          converted amount will be converted into your local currency according
+          to the exchange rate of credit card associations. As a result of this
+          conversion there is a possibility of a slight difference from the
+          original price stated in our web site."
+        </p>
       </div>
 
       <div className='space-y-3'>
