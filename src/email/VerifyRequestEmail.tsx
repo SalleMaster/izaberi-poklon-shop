4edd7759dee48feb8d84b6500e63dfbac9b2e3fe
SalleMaster <salle90ar@gmail.com>
2025-06-18 13:44:06 +0200

    email update

--- src/email/VerifyRequestEmail.tsx
+++ src/email/VerifyRequestEmail.tsx
@@ -25,12 +25,22 @@ export default function VerifyRequestEmail({ url }: VerifyRequestEmailProps) {
     <Html>
       <Preview>Email verifikacija</Preview>
       <Tailwind>
-        <Head />
+        <Head>
+          <meta name='x-apple-data-detectors' content='true' />
+          <meta name='googlemail:nothreaded' content='true' />
+          <meta name='x-gmail-data-detectors' content='true' />
+        </Head>
+        <Preview>Verifikacija Vašeg naloga - Izaberi Poklon</Preview>
         <Body className='font-sans bg-white'>
           <Container className='max-w-xl'>
             <EmailHeader />
             <Section className='mb-[100px]'>
               <Text>Poštovani/a</Text>
+
+              <Text>
+                Potrebno je da verifikujete Vašu email adresu za pristup nalogu
+                na Izaberi Poklon sajtu.
+              </Text>
 
               <Text>
                 Molimo Vas da kliknete na dugme ispod kako biste se prijavili na
@@ -41,12 +51,18 @@ export default function VerifyRequestEmail({ url }: VerifyRequestEmailProps) {
                 href={url}
                 className='inline-block p-2 bg-black text-white rounded-lg text-sm my-5'
               >
-                Prijava
+                Kliknite ovde za verifikaciju i prijavu
               </Link>
 
               <Text>
-                Ukoliko Vi niste zatražili ovu verifikaciju, slobodno ignorišite
-                ovaj email.
+                Link je validan 24 sata. Ukoliko Vi niste zatražili ovu
+                verifikaciju, molimo Vas da ignorišete ovaj email.
+              </Text>
+
+              <Text>
+                Srdačan pozdrav,
+                <br />
+                Tim Izaberi Poklon
               </Text>
             </Section>
             <EmailFooter />
