7927e8d2e80985373679d79fb5911fad85ba94a9
SalleMaster <salle90ar@gmail.com>
2026-02-05 22:33:13 +0100

    initial update

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,18 @@
-import { PrismaClient } from '@/generated/prisma'
+// import { PrismaClient } from '@/generated/prisma/client'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma }
+// const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export default prisma
+// export default prisma
+
+import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+export const prisma = new PrismaClient({ adapter })
