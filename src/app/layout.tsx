1bb7fd9f3426acc55211da16fa5d8a7439f1eac6
SalleMaster <salle90ar@gmail.com>
2026-03-26 15:20:22 +0100

    crisp integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,13 @@ export default function RootLayout({
           strategy='afterInteractive'
           defer
         />
+        <Script
+          id='crisp-script'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="${process.env.NEXT_PUBLIC_CRISP_WEBSITE_ID}";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen group`}
