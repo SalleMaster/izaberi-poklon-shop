2ee4eedc9dff161e65cfa5c1bbcbef69428f5af3
SalleMaster <salle90ar@gmail.com>
2026-03-27 09:51:19 +0100

    rebase with stage and latest prisma install

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
-console.log(
-  'DATABASE_URL:',
-  process.env.DATABASE_URL,
-  process.env.DATABASE_URL ? '✅' : '❌',
-)
-
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient
 }
